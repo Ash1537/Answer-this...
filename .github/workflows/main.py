51226e332756e2d95466b84d5cc8350ab2d51bd8
Ash <99724162+Ash1537@users.noreply.github.com>
2023-01-23 16:09:46 +0630

    Delete main.py

--- .github/workflows/main.py
+++
@@ -1,9 +0,0 @@
-value = str(input("Do you believe I'm in love with you? Y/N? : "))
-value=="Y"
-while value=="Y" :
-    print("YESSSS! I FUCKING LOVE YOU ♡")
-
-if value == "N":
-    print ()
-    print ("NO YOU CAN'T CHOOSE N!")
-    print ("CHOOSE Y >.< ")
